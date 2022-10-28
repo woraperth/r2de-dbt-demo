87ab016549c3333e7cce2d5ae9998197db8fa2a1
Perth Ngarmtrakulchol <hotwora@gmail.com>
2022-10-28 09:58:29 +0000

    Add new line

--- models/sales_table.sql
+++ models/sales_table.sql
@@ -1,1 +1,2 @@
-SELECT *, (Quantity_Ordered * Price_Each) AS total_price FROM `road-to-data-engineer-sql-db.dbtdemo.sales`
+SELECT *, (Quantity_Ordered * Price_Each) AS total_price
+FROM `road-to-data-engineer-sql-db.dbtdemo.sales`
